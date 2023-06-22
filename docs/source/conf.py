d8b4f55b1b4d0debab0e01a22aee97681526227a
Bryan Brzycki <bbrzycki@berkeley.edu>
2023-06-21 18:13:09 -0700

    Fix path and add rtd yaml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,8 +9,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
-exec(open('jort/_version.py').read())
+dirname = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(dirname, '../../'))
+exec(open(os.path.join(dirname, '../../jort/_version.py')).read())
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
